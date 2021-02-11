1899fd89454bc8d7bb6f21ebb09080c0353965b7
facuruku <facu.meza.lg@gmail.com>
2021-02-11 21:03:39 +0000

    Configurado gulp-sass y Browser-sync

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,33 @@
-function defaultTask(cb) {
-    // place code for your default task here
-    console.log("Probando gulp");
-    cb();
-}
+'use strict';
 
-exports.default = defaultTask
+var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
+var sass = require('gulp-sass');
+
+sass.compiler = require('node-sass');
+
+
+// Compile sass into CSS & auto-inject into browserSync
+gulp.task('sass', function () {
+    return gulp.src('./css/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./css'))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('sass:watch', function(){
+    gulp.watch('./css/*.scss', gulp.series(['sass']));
+});
+
+// Static Server + watching scss/html files
+gulp.task('serve', gulp.series(['sass'], function () {
+
+    browserSync.init({
+        server: "./"
+    });
+
+    gulp.watch("./css/*.scss", gulp.series(['sass']));
+    gulp.watch("./index.html").on('change', browserSync.reload);
+}));
+
+gulp.task('default', gulp.series(['serve']));
